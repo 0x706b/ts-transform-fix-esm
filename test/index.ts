719f3233c0d3bd84c01d736bf55060e78048680e
0x706b <peter@peterkrol.com>
2020-08-30 21:02:16 -0400

    Remove temporary test

--- test/index.ts
+++
@@ -1,9 +0,0 @@
-import * as A from "fp-ts/lib/Array";
-import * as Rx from "@0x706b/convenience-ts/Record";
-import * as T from "@effect-ts/core/Effect";
-import { something } from "./folder";
-
-console.log(A.map);
-console.log(Rx.map_);
-console.log(T.map_);
-console.log(something);
